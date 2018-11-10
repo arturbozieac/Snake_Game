38236495c63e81aeaf9fe8a37835cf0ebf6ab72c
Chandler <barty1215@gmail.com>
2018-11-10 12:04:58 +0200

    Learned about Encapsulation, reduced lines of code.

--- Snake_Game/Point.cs
+++ Snake_Game/Point.cs
@@ -12,6 +12,12 @@ namespace Snake_Game
         public int y;
         public char sym;
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
             public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake_Game/Program.cs
+++ Snake_Game/Program.cs
@@ -10,39 +10,22 @@ namespace Snake_Game
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
+            Point p1 = new Point(1, 3, '*');
+            /*p1.x = 1;
             p1.y = 3;
-            p1.sym = '*';
+            p1.sym = '*'; */
             p1.Draw();
 
-            //Draw(p1.x, p1.y, p1.sym);
-
-            /* int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1); */
-
-            Point p2 = new Point();
-            p2.x = 4;
+            Point p2 = new Point(4, 5, '#');
+            /*p2.x = 4;
             p2.y = 5;
-            p2.sym = '#';
+            p2.sym = '#'; */
             p2.Draw();
            
-            /* int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2); */
-
-
+          
             Console.ReadLine();
 
-           /*  void Draw(int x, int y, char sym)
-            {
-                Console.SetCursorPosition(x, y);
-                Console.Write(sym);
-            } */
+       
         }
     }
 }
