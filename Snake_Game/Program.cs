ea52ec96f99fc6a4716a0264906e1124d24e4d73
Chandler <barty1215@gmail.com>
2018-11-11 21:44:56 +0200

    add smth

--- Snake_Game/Program.cs
+++ Snake_Game/Program.cs
@@ -11,18 +11,38 @@ namespace Snake_Game
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*'; */
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            /*p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#'; */
             p2.Draw();
-           
-          
+
+            Point p3 = new Point(6, 8, '%');
+            p3.Draw();
+
+            Point p4 = new Point(8, 10, '$');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            numList.RemoveAt(0);
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+
             Console.ReadLine();
 
        
