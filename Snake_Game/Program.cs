1f5e55af0f502d37f9e6f348df090c32eb960f41
Chandler <barty1215@gmail.com>
2018-11-16 18:49:34 +0200

    We created lists of point and using them made vertical and horisontal lines which will be our game's space

--- Snake_Game/Program.cs
+++ Snake_Game/Program.cs
@@ -9,39 +9,17 @@ namespace Snake_Game
     class Program
     {
         static void Main(string[] args)
-        {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+        { 
+            HorizontalLine topLine = new HorizontalLine(1, 30, 1, '_');
+            HorizontalLine bottomLine = new HorizontalLine(1, 30, 20, '_');
+            topLine.Drow();
+            bottomLine.Drow();
+            
+            VerticalLine leftLine = new VerticalLine(2, 20, 1, '|');
+            VerticalLine rightLine = new VerticalLine(2, 20, 30, '|');
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            Point p3 = new Point(6, 8, '%');
-            p3.Draw();
-
-            Point p4 = new Point(8, 10, '$');
-            p4.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            numList.RemoveAt(0);
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
+            leftLine.Drow();
+            rightLine.Drow();
 
             Console.ReadLine();
 
---
+++ Snake_Game/VerticalLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int  y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+
+            }
+
+
+
+        }
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
